113bae61bd944ee18a456214c676e15140ec7de2
jinyang1994 <liujinyang@bigtiger.me>
2019-07-15 17:47:56 +0800

    chore: add gulp task display name

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,4 +1,3 @@
-'use strict';
 const path = require('path');
 const gulp = require('gulp');
 const webpack = require('webpack');
@@ -17,70 +16,62 @@ const webpackConfig = require('./webpack.config');
 const esmLib = path.join(process.cwd(), 'esm');
 const cjsLib = path.join(process.cwd(), 'cjs');
 const distLib = path.join(process.cwd(), 'dist');
+const siteDir = path.join(process.cwd(), '_site');
 
 function compileTs(output) {
-  const modules = output === esmLib ? false : undefined;
-  const babelConfig = getBabelConfig(modules);
+  const task = (done) => {
+    const modules = output === esmLib ? false : undefined;
+    const babelConfig = getBabelConfig(modules);
+    const { js, dts } = gulp
+      .src([
+        'components/**/*.tsx',
+        'components/**/*.ts',
+        'typings/**/*.d.ts',
+      ])
+      .pipe(ts(tsConfig.compilerOptions));
 
-  const { js, dts } = gulp
-    .src([
-      'components/**/*.tsx',
-      'components/**/*.ts',
-      'typings/**/*.d.ts',
-    ])
-    .pipe(ts(tsConfig.compilerOptions));
+    return merge2([
+      js.pipe(babel(babelConfig)).pipe(gulp.dest(output)),
+      dts.pipe(gulp.dest(output)),
+    ]).on('finish', done);
+  };
 
-  return merge2([
-    js.pipe(babel(babelConfig)).pipe(gulp.dest(output)),
-    dts.pipe(gulp.dest(output)),
-  ]);
+  task.displayName = `Compile Typescript to ${output === esmLib ? 'ES Module' : 'CommonJS'}`;
+
+  return task;
 }
 
 function compileLess(output) {
-  return gulp
-    .src(['components/**/index.less'])
-    .pipe(less())
-    .pipe(postcss(postcssConfig.plugins))
-    .pipe(gulp.dest(output));
+  const task = () => {
+    return gulp
+      .src(['components/**/index.less'])
+      .pipe(less())
+      .pipe(postcss(postcssConfig.plugins))
+      .pipe(gulp.dest(output));
+  };
+
+  task.displayName = `Compile mode ${output === esmLib ? 'ES Module' : 'CommonJS'}'s Less to CSS`;
+
+  return task;
 }
 
 function clean(...paths) {
-  Array.from(paths).forEach((path) => {
-    rimraf.sync(path);
-  });
+  const task = (done) => {
+    Array.from(paths).forEach((item) => {
+      rimraf.sync(item);
+    });
+    done();
+  };
+
+  task.displayName = `Clean ${paths.toString()}`;
+
+  return task;
 }
 
-gulp.task('compile:esm', gulp.series(
-  (done) => {
-    clean(esmLib);
-    done();
-  },
-  gulp.parallel(
-    (done) => compileTs(esmLib).on('finish', done),
-    () => compileLess(esmLib),
-  ),
-));
-
-gulp.task('compile:cjs', gulp.series(
-  (done) => {
-    clean(cjsLib);
-    done();
-  },
-  gulp.parallel(
-    (done) => compileTs(cjsLib).on('finish', done),
-    () => compileLess(cjsLib),
-  ),
-));
-
-gulp.task('compile', gulp.parallel('compile:esm', 'compile:cjs'));
-
-gulp.task('dist', gulp.series(
-  (done) => {
-    clean(distLib);
-    done();
-  },
-  (done) => {
+function dist() {
+  const task = (done) => {
     webpack(webpackConfig, (err, stats) => {
+      /* eslint-disable no-console */
       if (err) {
         console.error(err.stack || err);
         if (err.details) {
@@ -112,10 +103,34 @@ gulp.task('dist', gulp.series(
 
       done();
     });
-  },
+  };
+
+  task.displayName = 'Use webpack build UMD product';
+
+  return task;
+}
+
+gulp.task('compile:esm', gulp.series(
+  clean(esmLib),
+  gulp.parallel(
+    compileTs(esmLib),
+    compileLess(esmLib),
+  ),
 ));
 
-gulp.task('clean', (done) => {
-  clean(esmLib, cjsLib, distLib, '_site');
-  done();
-});
+gulp.task('compile:cjs', gulp.series(
+  clean(cjsLib),
+  gulp.parallel(
+    compileTs(cjsLib),
+    compileLess(cjsLib),
+  ),
+));
+
+gulp.task('compile', gulp.parallel('compile:esm', 'compile:cjs'));
+
+gulp.task('dist', gulp.series(
+  clean(distLib),
+  dist(),
+));
+
+gulp.task('clean', clean(esmLib, cjsLib, distLib, siteDir));
