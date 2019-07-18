6747a6c867429d3c196201774412702217ff851d
jinyang1994 <liujinyang@bigtiger.me>
2019-07-18 22:20:01 +0800

    feat: add button icon and loading attribute

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,6 +1,8 @@
-import React, { ButtonHTMLAttributes } from 'react';
+import React, { ButtonHTMLAttributes, Children, ReactChild } from 'react';
 import classNames from 'classnames';
-import { getPrefixCls } from '../utils/config';
+import Icon from '../icon';
+import { getPrefixCls } from '../_utils/config';
+import { isReactText } from '../_utils/children';
 
 const prefixCls = getPrefixCls('btn');
 
@@ -8,14 +10,40 @@ interface Props extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'type'> {
   type?: 'primary' | 'success' | 'info' | 'warning' | 'danger';
   htmlType?: 'button' | 'submit' | 'reset';
   fill?: boolean;
+  icon?: string;
+  loading?: string;
+  children?: ReactChild[];
 }
 
 function Button(props: Props) {
-  const { children, className, htmlType, type, fill, ...btnProps } = props;
+  const { children, className, htmlType, type, fill, icon, loading, ...btnProps } = props;
   const classes = classNames(prefixCls, className, {
     [`${prefixCls}-${type}`]: !!type,
     [`${prefixCls}-fill`]: fill,
+    [`${prefixCls}-loading`]: loading,
   });
+  const nodes: ReactChild[] = [];
+  let isPrevReactText = false;
+
+  if (loading) {
+    nodes[0] = <Icon name="loading" className={`${prefixCls}-icon`} spin={0.8} />;
+  } else if (icon) {
+    nodes[0] = <Icon name={icon} className={`${prefixCls}-icon`} />;
+  }
+  if (children) {
+    Children.forEach(children, (child) => {
+      const isCurrentReactText = isReactText(child);
+
+      if (isPrevReactText && isCurrentReactText) {
+        const lastIndex = nodes.length - 1;
+
+        nodes[lastIndex] = `${nodes[lastIndex]}${child}`;
+      } else {
+        nodes.push(child);
+      }
+      isPrevReactText = isCurrentReactText;
+    });
+  }
 
   return (
     <button
@@ -23,7 +51,7 @@ function Button(props: Props) {
       className={classes}
       {...btnProps}
     >
-      <span>{children}</span>
+      {Children.map(nodes, (node) => isReactText(node) ? <span>{node}</span> : node)}
     </button>
   );
 }
